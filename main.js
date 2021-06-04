f1c8175524f88b930edefb089d7f86f9dae030a8
TaraShevchenko <77995009+TaraShevchenko@users.noreply.github.com>
2021-06-04 20:17:52 +0300

    Homework-3
    На прошлом уроке ты исправил в объектах игроков attack(): { ... } на attack: function() {...}. Я не совсем понял почему, ведь я ее могу и позвать и просто передать без вызова. Почему первый вариант не будет работать ?

--- main.js
+++ main.js
@@ -1,26 +1,29 @@
-const Scorpion = {
+const Player1 = {
+    player: 'player1',
     name: 'Scorpion',
     hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
     weapon: [
         'Sword', 'Katana'
     ],
-    attack() {
+    attack: function () {
         console.log(this.name + ' Fight');
     }
 }
 
-const Subzero = {
+const Player2 = {
+    player: 'player2',
     name: 'Subzero',
-    hp: 50,
+    hp: 100,
     img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
     weapon: [
         'Sword', 'Katana'
     ],
-    attack() {
+    attack: function () {
         console.log(this.name + ' Fight');
     }
 }
+
 const createElement = (elem, className, parent) => {
     if (elem) {
         let element = document.createElement(elem);
@@ -33,18 +36,54 @@ const createElement = (elem, className, parent) => {
         return element;
     }
 }
-const createPlayer = (playerId, playerInfo) => {
-    const arenas = document.querySelector('.root .arenas');
-    const player = createElement('div', playerId, arenas);
+
+const arenas = document.querySelector('.arenas');
+const randomButton = document.querySelector('.button');
+
+const createPlayer = ($player) => {
+    const player = createElement('div', $player.player, arenas);
     const progressbar = createElement('div', 'progressbar', player);
     const life = createElement('div', 'life', progressbar);
-    life.style.cssText = `width: ${playerInfo.hp}%;`;
+    life.style.cssText = `width: ${$player.hp}%;`;
     const name = createElement('div', 'name', progressbar);
-    name.innerText = playerInfo.name;
+    name.innerText = $player.name;
     const character = createElement('div', 'character', player);
     const characterImage = createElement('img', '', character);
-    characterImage.setAttribute("src", playerInfo.img)
+    characterImage.setAttribute("src", $player.img)
 }
 
-createPlayer('player1', Scorpion);
-createPlayer('player2', Subzero);
+const winner = ($player) => {
+    for (let i = 0; i < $player.length; i++) {
+        if ($player[i].hp > 0) {
+            return $player[i].name;
+        }
+    }
+}
+
+const fightResult = () => {
+    const fightResultPanel = createElement('div', 'loseTitle', arenas);
+    fightResultPanel.innerText = winner([Player1, Player2]) + ' Wins';
+
+    randomButton.setAttribute('disabled', 'disabled');
+}
+
+const onRandomClick = ($player) => {
+    for (let i = 0; i < $player.length; i++) {
+        const playerHp = document.querySelector(`.${$player[i].player} .life`);
+
+        $player[i].hp = $player[i].hp - Math.ceil(Math.random() * 20);
+        if ($player[i].hp > 0) {
+            playerHp.style.cssText = `width: ${$player[i].hp}%;`;
+        } else {
+            playerHp.style.cssText = 'width: 0%;';
+            fightResult();
+        }
+    }
+}
+
+randomButton.addEventListener('click', function () {
+    onRandomClick([Player1, Player2]);
+});
+
+createPlayer(Player1);
+createPlayer(Player2);
